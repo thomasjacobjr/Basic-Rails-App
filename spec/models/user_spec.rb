646a07948d0942a30d40883d7ec02e0ffb461685
thomasjacobjr <thomasjacobjr@gmail.com>
2016-01-20 16:59:46 -0700

    Assignment 36 Rails Authentication User Model: Completed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,6 +27,12 @@ RSpec.describe User, type: :model do
        it "should respond to email" do
          expect(user).to respond_to(:email)
        end
+
+       it "should format the user's name" do
+         user.name = "lorbo jenkins"
+         user.save
+         expect(user.name). to eq "Lorbo Jenkins"
+       end
      end
 
      describe "invalid user" do
@@ -45,6 +51,6 @@ RSpec.describe User, type: :model do
         it "should be an invalid user due to incorrectly formatted email address" do
           expect(user_with_invalid_email_format).to_not be_valid
         end
-      end     
+      end
 
 end
