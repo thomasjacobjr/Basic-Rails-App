82b6b9b1ba43ba30efa8658f4dc8c7a94ef61a76
thomasjacobjr <thomasjacobjr@gmail.com>
2016-01-21 16:52:14 -0700

    Checkpoint 40 Rails Authorization: Completed (I think?)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160121223431) do
+ActiveRecord::Schema.define(version: 20160121232433) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20160121223431) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "role"
   end
 
 end
